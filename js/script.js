9d44aa70f5cbb46a6bddc634f476b1f19351fe29
rupparna <rupparna3@gmail.com>
2026-08-06 01:40:49 +0530

    redesigning ui

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ startButton.addEventListener("click", () => {
     home.style.display = "none";
     roles.style.display = "block";
 });
+
+const roleCards = document.querySelectorAll(".role-card");
+let selectedRole = "";
+roleCards.forEach((card) => {
+    card.addEventListener("click", () => {
+        selectedRole = card.id;
+        console.log("Selected role:", selectedRole);
+    });
+});
